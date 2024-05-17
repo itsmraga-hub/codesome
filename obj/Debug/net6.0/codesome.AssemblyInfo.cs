b31ab8828b1c4c22d43175320b4c32a3b75bcd10
itsmraga-hub <itsragamit@gmail.com>
2024-05-17 12:25:31 +0300

    Merge

--- obj/Debug/net6.0/codesome.AssemblyInfo.cs
+++ obj/Debug/net6.0/codesome.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("codesome")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0beb421b693b70819c8867989258adb7d898fe5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("codesome")]
 [assembly: System.Reflection.AssemblyTitleAttribute("codesome")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
